3e208cfebc39c1dc3b97690af97986fc3570fc6b
Jorge Rojo <jorge.rojodiseno@gmail.com>
2019-03-03 00:09:11 +0100

    Battery level added

--- ESP32/Polytt/Battery.h
+++ ESP32/Polytt/Battery.h
@@ -1,21 +1,34 @@
-class Battery { 
-	private:
-		byte _pin;
-		unsigned int _raw;
-		float _max; 
-		float _res;
+#define BATT_MAX_RAW 2320
 
-	public:
-		Battery(byte pin, float batteryMaxVolt, float resistanceDivider) {
-			this->_pin = pin;
-			this->_max = batteryMaxVolt;
-			this->_res = resistanceDivider; 
-			pinMode(pin, INPUT);
-		} 
 
-		float volt() {     
-      		_raw = float(analogRead(this->_pin)); 
-			return ((this->_max / this->_res) * (_raw / 1023));
-		}  
+class Battery {
+
+  private:
+    byte _pin;
+    unsigned int _raw;
+    float _last = 0;
+
+  public:
+    Battery(byte pin) {
+      this->_pin = pin;
+      pinMode(pin, INPUT);
+    }
+
+    float level() {
+      _raw = analogRead(this->_pin);
+      float level = (100 * _raw) / BATT_MAX_RAW;
+
+      if ( this->_last < level && (level - this->_last) > 1 ) {
+        this->_last += 1;
+      }
+      else if ( this->_last > level && (this->_last - level) > 1 ) {
+        this->_last -= 1;
+      }
+      else {
+        this->_last = level;
+      }
+
+      return this->_last;
+    }
+
 };
- 
--- ESP32/Polytt/Polytt.ino
+++ ESP32/Polytt/Polytt.ino
@@ -1,45 +1,41 @@
 #include "Arduino.h"
-#include "./Storage.h" 
-#include "./Battery.h" 
-#include "./Orientation.h"  
-#define PIN_BATTERY A0 
-#define PIN_LED 2  
+#include "./Storage.h"
+#include "./Battery.h"
+#include "./Orientation.h"
+#define PIN_BATTERY A0
+#define PIN_LED 2
 
-Storage storage = Storage(true);
-Battery battery = Battery(PIN_BATTERY, 4.17, 2);
+Battery battery = Battery(PIN_BATTERY);
+Storage storage = Storage(false);
 Orientation orientation = Orientation(PIN_LED, storage);
 
-
- 
-
 void setup()
-{  
-	Serial.begin(115200);
-	delay(500);
-  
-	
-	orientation.setup(); 
+{
+  Serial.begin(115200);
+  delay(500);
+
+  orientation.setup();
+  delay(500);
 }
-  
+
 void loop()
-{  
-	// volts  
-	Serial.print("Volts:\t");
-	Serial.print(battery.volt());  
+{
 
- 
-	// orientation
-  	orientation.loop(); 
+  // orientation
+  orientation.loop();
+  float* ypr = orientation.get_ypr();
+
+  Serial.print("YPR\t");
+  Serial.print(ypr[0] * 180 / M_PI);
+  Serial.print("\t");
+  Serial.print(ypr[1] * 180 / M_PI);
+  Serial.print("\t");
+  Serial.print(ypr[2] * 180 / M_PI);
+  Serial.print("\t\t");
 
 
-	Serial.println("");
-
-	//delay(200);
-
-  
+  // volts
+  float level = battery.level();
+  Serial.println(level);
+  delay(100);
 }
- 
-
- 
-
- 
